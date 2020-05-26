193dc32e12cb1e14b3e8a0318eaf93292a8b8a8b
Sharath Sriram <sharath.sriram@freshworks.com>
2020-05-26 11:15:22 +0530

    Initial Commit

---
+++ ICICI Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICICI Sample
+//
+//  Created by Sharath Sriram on 26/05/20.
+//  Copyright © 2020 Sharath Sriram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
